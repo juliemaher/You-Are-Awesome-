9df7403aa1f645d289695994c37019a939653dea
Julie Maher <maherjl@bc.edu>
2020-12-27 15:59:53 -0500

    You Are Awesome- with resizing

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageLabel.text=""
+        
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
