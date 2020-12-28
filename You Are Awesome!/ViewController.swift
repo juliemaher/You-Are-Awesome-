21ac27e05384f6694dd7d6153230125b08257c82
Julie Maher <maherjl@bc.edu>
2020-12-28 13:14:28 -0500

    Conditionals and interpolation

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -11,17 +11,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        messageLabel.text=""
-        
     }
-
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        messageLabel.text="You Are Awesome!" //property and atribute are the same thing
-        imageView.image=UIImage(named: "image0")
+        
+        print(imageNumber)
+        //let imageName = "image" + String(imageNumber)
+        let imageName = "image\(imageNumber)" //string interpolation
+        imageNumber = imageNumber+1
+        imageView.image=UIImage(named:imageName)
+        if imageNumber == 10 {
+            imageNumber = 0
+            
+            //        let awesomeMessage = "You Are Awesome!"
+            //        let greatMessage = "You Are Great!"
+            //        let bombMessage = "You Are Da Bomb!" 
+            //
+            //        if messageLabel.text == awesomeMessage {
+            //            messageLabel.text = greatMessage
+            //            imageView.image=UIImage(named: "image1")
+            //
+            //        } else if messageLabel.text == greatMessage {
+            //            messageLabel.text = bombMessage //property and atribute are the same thing
+            //            imageView.image=UIImage(named: "image2")
+            //
+            //        } else {
+            //            messageLabel.text = awesomeMessage //property and atribute are the same thing
+            //            imageView.image=UIImage(named: "image0")
+            //        }
+            
+        }
         
     }
-    
 }
 
